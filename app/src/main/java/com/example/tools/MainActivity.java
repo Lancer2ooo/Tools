77374caedbcf1f7dda3a84e6f2882bce08c2cec6
Lancer2ooo <1663639411@qq.com>
2021-09-26 11:37:02 +0800

    一个不断更新功能的工具合集！

--- app/src/main/java/com/example/tools/MainActivity.java
+++ app/src/main/java/com/example/tools/MainActivity.java
@@ -3,12 +3,125 @@ package com.example.tools;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextClock;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.request.RequestOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置应用全屏显示 ^^
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+
+        imageView = findViewById(R.id.back_img);
+        sendRequestWithHttpURLConnection();
     }
+
+    private void sendRequestWithHttpURLConnection() {
+        // 开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    // 下面对获取到的输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    parseJSONWithJSONObject(response.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    //    JSON的解析
+    private void parseJSONWithJSONObject(String jsonData) {
+        try {
+            // JSONArray jsonArray = new JSONArray(jsonData);
+
+            JSONArray jsonArray = new JSONObject(jsonData).getJSONArray("images");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String url = jsonObject.getString("url");
+
+                Log.d("MainActivity", "url is " + url);
+                String url1="http://cn.bing.com"+url;
+                showResponse(url1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showResponse(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                //毛玻璃效果（内存溢出bug）
+                MultiTransformation multiTransformation = new MultiTransformation(
+                        new BlurTransformation(MainActivity.this,25)
+                );
+
+                // 在这里进行UI操作，将结果显示到界面上
+                Glide.with(MainActivity.this)
+                        .load(response)
+                        .centerCrop()
+                        .dontAnimate()
+                        .into(imageView);
+            }
+        });
+    }
+
+
 }
